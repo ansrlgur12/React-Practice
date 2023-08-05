96e09a6766cfb6eef59c0f66fe5cb961a97fa9eb
KIHYUKMOON <121914919+ansrlgur12@users.noreply.github.com>
2023-08-05 23:58:06 +0900

    로그인 기본

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
@@ -11,9 +11,31 @@ const LoginContainer = styled.div`
 
 const Login = () => {
     const nav = useNavigate();
+    const [loginId, setLoginId] = useState("");
+    const [loginPwd, setLoginPwd] = useState("");
+
+    const onChangeId = (e) => {
+        setLoginId(e.target.value);
+        console.log(e.target.value);
+    }
+
+    const onChangePwd = (e) => {
+        setLoginPwd(e.target.value);
+        console.log(e.target.value);
+    }
+
+    const onClickLogin = () => {
+        console.log("로그인 버튼이 클릭되었습니다.")
+        console.log(`아이디 : ${loginId}`);
+        console.log(`비밀번호 : ${loginPwd}`);
+    }
+
     return(
         <LoginContainer>
             <p>여기는 로그인 페이지 입니다.</p>
+            <div><input type="text" placeholder="아이디를 입력하세요" onChange={onChangeId}/></div>
+            <div><input type="password" placeholder="비밀번호를 입력하세요" onChange={onChangePwd}/></div>
+            <div><button onClick={onClickLogin}>로그인</button></div>
             <button onClick={()=>nav("/")}>Home</button>
         </LoginContainer>
     )
